862dc74de85e27552ffe51193f186e5118cb6557
kornjacaRade <radisic.rade@gmail.com>
2015-06-25 05:38:28 +0200

    Moguce je dobaviti i prikazati sve fakture, dok za pojedinacne je potrebno uraditi HTML prikaz, inace su dobavljene.

---
+++ FirmClient/app/scripts/controllers/invoiceController.js
@@ -0,0 +1,19 @@
+'use strict';
+
+ angular.module('invoice', ['resource.invoice', 'invoices', 'resource.invoiceParams'])
+ 	.controller('InvoiceCtrl', function(Invoice, InvoiceParams, $scope, $log) {
+ 		
+ 		$log.info(InvoiceParams.idFakture);
+ 		$log.info(InvoiceParams.idDobavljaca);
+ 		
+ 		Invoice.get({idDobavljaca : InvoiceParams.idDobavljaca, idFakture : InvoiceParams.idFakture}, function(response) {
+			$log.info(response);
+			
+			$scope.zaglavlje = response.zaglavlje;
+			$scope.stavkeFakture = response.stavkeFakture;
+			
+			$log.info("Successfully obtained invoice data from REST.");
+ 		});
+
+
+ 	});
