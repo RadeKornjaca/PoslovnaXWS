8c86167e59f52c83a5ec4b762d9d209678acb0a1
kornjacaRade <radisic.rade@gmail.com>
2015-06-26 16:06:07 +0200

    Valjda cu nauciti jednom. :D

---
+++ ClientAPI/src/entity/dobavljac/Partneri.java
@@ -0,0 +1,16 @@
+package entity.dobavljac;
+
+import java.util.List;
+
+public class Partneri {
+	private List<Dobavljac> partneri;
+
+	public List<Dobavljac> getPartneri() {
+		return partneri;
+	}
+
+	public void setPartneri(List<Dobavljac> partneri) {
+		this.partneri = partneri;
+	}
+
+}
